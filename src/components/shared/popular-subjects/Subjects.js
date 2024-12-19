96fcbbebf71420d915ca5ca032ff0380b918b0a9
propiter <pro.pedro0413@gmail.com>
2024-12-19 14:39:03 -0500

    updated informative tarjet popularSubjets Sections

--- src/components/shared/popular-subjects/Subjects.js
+++ src/components/shared/popular-subjects/Subjects.js
@@ -2,11 +2,11 @@ import React from "react";
 import Subject from "./Subject";
 
 
-const Subjects = ({ subjects, textLink }) => {
+const Subjects = ({ subjects }) => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-30px">
       {subjects.map((subject, idx) => (
-        <Subject   key={idx} subject={subject} textLink={textLink} />
+        <Subject   key={idx} subject={subject} />
       ))}
     </div>
   );
