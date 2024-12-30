7ce2f76c34b1f7d22b4a7a82dfb4bf88a5e2888b
propiter <pro.pedro0413@gmail.com>
2024-12-30 10:57:09 -0500

    clean landing page

--- src/components/layout/footer/FooterNavList.js
+++ src/components/layout/footer/FooterNavList.js
@@ -1,6 +1,4 @@
 import React from "react";
-import FooterAbout from "./FooterAbout";
-import FooterRecentPosts from "./FooterRecentPosts";
 
 const FooterNavList = () => {
   
@@ -8,10 +6,7 @@ const FooterNavList = () => {
   return (
     <section>
       <div className="grid grid-cols-1 sm:grid-cols-12 md:grid-cols-2 lg:grid-cols-12 gap-30px md:gap-y-5 lg:gap-y-0 pt-60px pb-50px md:pt-30px md:pb-30px lg:pt-110px lg:pb-20">
-        
-
-        {/* right */}
-        
+                
       </div>
     </section>
   );
